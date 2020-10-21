9f8023b4febec531ac851441e593a403d55fb501
Ru Escribano <escribanoruben@gmail.com>
2020-10-21 11:46:05 +0200

    fixed validators

--- src/app/contacts/pages/contact-form/contact-form.component.ts
+++ src/app/contacts/pages/contact-form/contact-form.component.ts
@@ -20,8 +20,8 @@ export class ContactFormComponent implements OnInit {
   TechnologyList = Object.keys(Technology);
 
   contactForm = this.fb.group({
-    name: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(80)]],
-    phoneNumber: [null, [Validators.required, Validators.maxLength(80)]],
+    name: [null, [Validators.required, Validators.minLength(3)]],
+    phoneNumber: [null, [Validators.required]],
     birthdate: [null, []],
     technologies: [null, []],
   });
@@ -40,13 +40,18 @@ export class ContactFormComponent implements OnInit {
     this.isAddMode = !this.contactId;
 
     if (!this.isAddMode) {
-      this.isLoading = true;
-      this.contactsService.getContactById(this.contactId).subscribe((contact) => {
-        const { id, ...contactWithId } = contact;
-        this.contactForm.setValue(contactWithId);
-        this.isLoading = false;
-      });
+      this.loadData();
     }
+  }
+
+  loadData() {
+    this.isLoading = true;
+    this.contactsService.getContactById(this.contactId).subscribe((contact) => {
+      const { id, ...contactWithId } = contact;
+      this.contactForm.setValue(contactWithId);
+      this.isLoading = false;
+      this.contactForm.markAsPristine();
+    });
   }
 
   createContact() {
@@ -71,7 +76,7 @@ export class ContactFormComponent implements OnInit {
         duration: 4000,
       });
       snackBarRef.onAction().subscribe(() => {
-        this.router.navigate(['/contacts', 'edit', contact.id]);
+        this.router.navigate(['/contacts', 'edit', this.contactId]);
       });
       this.router.navigate(['/contacts']);
     });
