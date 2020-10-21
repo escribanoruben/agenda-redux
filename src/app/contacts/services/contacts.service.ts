fda2a462b4311ef9393dac937f00ed2f53f877f2
Ru Escribano <escribanoruben@gmail.com>
2020-10-21 09:13:30 +0200

    added contactById method

--- src/app/contacts/services/contacts.service.ts
+++ src/app/contacts/services/contacts.service.ts
@@ -12,19 +12,21 @@ export class ContactsService {
 
   getAllContacts(): Observable<Contact[]> {
     const url = `${environment.baseUrl}/contacts`;
-
     return this.http.get<Contact[]>(url);
+  }
+
+  getContactById(contactId: number): Observable<Contact> {
+    const url = `${environment.baseUrl}/contacts/${contactId}`;
+    return this.http.get<Contact>(url);
   }
 
   createContact(contact: Contact): Observable<Contact> {
     const url = `${environment.baseUrl}/contacts`;
-
     return this.http.post<Contact>(url, contact);
   }
 
   updateContact(contact: Contact): Observable<Contact> {
     const url = `${environment.baseUrl}/contacts/${contact.id}`;
-
     return this.http.put<Contact>(url, contact);
   }
 }
